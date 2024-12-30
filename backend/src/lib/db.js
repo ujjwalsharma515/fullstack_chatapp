93e6f8ad6bf7f338ab021acf14f8fcbee5c0cf63
ujjwal <ujjwalsharma515@gmail.com>
2024-12-30 16:19:56 +0530

    mongodb_url

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
 
-    const conn = await mongoose.connect("mongodb+srv://user2002:attack2611@cluster0.slxyr.mongodb.net/db_name?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
